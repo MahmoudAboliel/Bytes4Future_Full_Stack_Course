7c90c487b37c11c8ccdd56c8d336bdc700bc52de
Mahmoud Abulail <mahmoudabulail1998@gmail.com>
2026-09-03 18:06:15 +0300

    build logic for handle radio, checkbox and textarea fields

--- frontend_section/session_3/lib.js
+++ frontend_section/session_3/lib.js
@@ -8,10 +8,63 @@ const generateId = (len = 16) => {
   return id;
 };
 
+const inputChange = (value, data, name) => (data[name] = value);
+
+const radioType = (obj = {}, data = {}) => {
+  const radioDiv = document.createElement("div");
+
+  obj.options.map((option) => {
+    const radioLabel = document.createElement("label");
+    const radioInput = document.createElement("input");
+    const radioId = generateId();
+    radioLabel.htmlFor = radioId;
+    radioLabel.innerHTML = option.label;
+    radioInput.id = radioId;
+    radioInput.type = obj.type;
+    radioInput.name = obj.name;
+    radioInput.value = option.value;
+    radioInput.addEventListener("input", (e) =>
+      inputChange(e.target.value, data, obj.name),
+    );
+    radioDiv.append(radioInput, radioLabel);
+  });
+
+  return radioDiv;
+};
+
+const checkboxType = (obj = {}, data = {}) => {
+  const checkboxDiv = document.createElement("div");
+  obj.options.map((option) => {
+    const id = generateId();
+    const optionDiv = document.createElement("div");
+    const input = document.createElement("input");
+    const label = document.createElement("label");
+    label.htmlFor = id;
+    label.innerHTML = option.label;
+
+    input.id = id;
+    input.type = obj.type;
+    input.name = obj.name;
+    input.value = option.value;
+    input.addEventListener("change", (e) => {
+      const item = e.target;
+      if (item.checked && !data[obj.name].includes(item.value))
+        data[obj.name].push(item.value);
+      else if (!item.checked && data[obj.name].includes(item.value))
+        data[obj.name].splice(data[obj.name].indexOf(item.value), 1);
+    });
+
+    optionDiv.append(input, label);
+    checkboxDiv.append(optionDiv);
+  });
+
+  return checkboxDiv;
+};
+
 const renderInput = (obj = {}, data) => {
   const attributes = Object.keys(obj);
   const div = document.createElement("div");
-  div.classList = "group-field";
+  div.classList.add("group-field");
 
   if (attributes.includes("name")) {
     // generate id
@@ -21,6 +74,46 @@ const renderInput = (obj = {}, data) => {
     // set attribute
     label.htmlFor = id;
     label.innerHTML = attributes.includes("label") ? obj.label : obj.name;
+
+    // radio type
+    if (obj.type == "radio") {
+      if (obj.options) {
+        const radioDiv = radioType(obj, data);
+        div.append(label, radioDiv);
+        return div;
+      } else {
+        div.innerHTML = "the field is wrong";
+        div.style.color = "red";
+        return div;
+      }
+    }
+    // textarea
+    if (obj.type == "textarea") {
+      const textarea = document.createElement("textarea");
+      textarea.id = id;
+      textarea.value = data[obj.name];
+      textarea.addEventListener("input", (e) =>
+        inputChange(e.target.value, data, obj.name),
+      );
+      attributes.map((item) => {
+        textarea[item] = obj[item];
+      });
+      div.append(label, textarea);
+      return div;
+    }
+
+    // checkbox
+    if (obj.type == "checkbox") {
+      if (obj.options) {
+        const checkboxDiv = checkboxType(obj, data);
+        div.append(label, checkboxDiv);
+        return div;
+      } else {
+        div.innerHTML = "the field is wrong";
+        div.style.color = "red";
+        return div;
+      }
+    }
 
     const input = document.createElement("input");
     input.id = id;
@@ -50,11 +143,8 @@ const renderForm = (id, fields = [], onSubmit) => {
 
   fields.map((field) => {
     if (field.name) {
-      if (field.type == "number" || field.type == "range") {
-        data[field.name] = field.defaultValue ?? field.min ?? 0;
-      } else {
-        data[field.name] = field.defaultValue ?? "";
-      }
+      if (field.type == "checkbox") data[field.name] = [];
+      else data[field.name] = field.value ?? field.defaultValue ?? null;
     }
     const myField = renderInput(field, data);
     form.appendChild(myField);
@@ -62,13 +152,13 @@ const renderForm = (id, fields = [], onSubmit) => {
 
   //   create buttons
   const div = document.createElement("div");
-  div.classList = "group-buttons";
+  div.classList.add("group-buttons");
 
   const submit = document.createElement("input");
   submit.type = "submit";
   submit.value = "send";
   submit.onclick = (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     onSubmit(data);
   };
 
@@ -76,22 +166,27 @@ const renderForm = (id, fields = [], onSubmit) => {
   cansel.type = "button";
   cansel.value = "cancel";
   cansel.onclick = (e) => {
+    // remove data from UI
     e.preventDefault();
     const myData = Array.from(form.querySelectorAll("input"));
 
     for (let i in myData) {
       if (!["submit", "button"].includes(myData[i].type)) {
-        if (myData[i].type == "number" || myData[i].type == "range") {
-          myData[i].value = myData[i].defaultValue ?? myData[i].min ?? 0;
+        if (myData[i].type == "range") {
+          myData[i].value = myData[i].min;
+        } else if (myData[i].type == "radio" || myData[i].type == "checkbox") {
+          myData[i].checked = false;
         } else {
-          myData[i].value = myData[i].defaultValue ?? "";
+          myData[i].value = myData[i].defaultValue ?? null;
         }
       }
     }
 
+    // remove data from back
     for (let key in data) {
-      if (data.type == "number" || data.type == "range") data[key] == 0;
-      else data[key] = "";
+      const field = fields.find((field) => field.name == key);
+      if (field.type == "checkbox") data[key] = [];
+      else data[key] = null;
     }
   };
 
