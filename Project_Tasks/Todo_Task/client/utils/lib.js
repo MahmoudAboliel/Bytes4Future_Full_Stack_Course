fd1e0a8e19790d387ffcb266d2ff5ceb725819da
Mahmoud Abulail <mahmoudabulail1998@gmail.com>
2026-09-22 21:18:38 +0300

    inhance files

--- Project_Tasks/Todo_Task/client/utils/lib.js
+++ Project_Tasks/Todo_Task/client/utils/lib.js
@@ -70,37 +70,6 @@ export const createCard = (task = {}) => {
   description.classList.add("card-text");
   description.innerHTML = task.description;
 
-  const accordionDiv = document.createElement("div");
-  accordionDiv.classList.add("accordion", "accordion-flush");
-  accordionDiv.style.margin = "8px 0";
-  accordionDiv.id = `accordionExample`;
-  accordionDiv.innerHTML = `
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne${task.id}" aria-expanded="false" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="collapseOne${task.id}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item’s accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo${task.id}" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo${task.id}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the second item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  `;
-
   // status & priority
   const statusDiv = document.createElement("div");
   statusDiv.classList.add("d-flex", "gap-1");
@@ -170,14 +139,13 @@ export const createCard = (task = {}) => {
     img.alt = user.avatarUrl.split("/").at(-1);
 
     img.addEventListener("mouseenter", () => {
-      fullDetails.classList.remove("hidden")
+      fullDetails.classList.remove("hidden");
     });
     img.addEventListener("mouseleave", () => {
-      fullDetails.classList.add("hidden")
+      fullDetails.classList.add("hidden");
     });
-    
-    imgsDiv.append(img, fullDetails);
 
+    imgsDiv.append(img, fullDetails);
   });
 
   cardFooter.append(creatorDiv, imgsDiv);
@@ -204,12 +172,12 @@ export const initializeTask = async (task = {}) => {
   const assigneeUsers = [];
   for (let i in task.assigneeIds) {
     const init = await getUserById(task.assigneeIds[i]);
-    assigneeUsers.push(init);
+    assigneeUsers.push({ ...init, avatarUrl: init.avatarUrl.slice(1) });
   }
 
   return {
     ...task,
-    creatorId: creator,
+    creatorId: { ...creator, avatarUrl: creator.avatarUrl.slice(1) },
     assigneeIds: assigneeUsers,
   };
 };
